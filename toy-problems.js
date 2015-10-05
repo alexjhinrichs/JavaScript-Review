be1ad04047fc438f8115701d7352732ce69631b7
Alex Hinrichs <alexjhinrichs@gmail.com>
2015-10-05 17:29:30 -0600

    Completely Finished

--- toy-problems.js
+++ toy-problems.js
@@ -34,29 +34,28 @@ function flatten(array) {
     var newArray = [];
     for(var i = 0; i < separate.length; i++) {
         if(i % 2 === 0) {
-        	var tostring = separate[i].toString();
-        	var tonumber = Number(tostring);
-            newArray.push(tonumber);
-            
+            var toNumber = Number(separate[i]);
+            newArray.push(toNumber);
         }
     }
+    
     return newArray;
 }
 
 flatten(multDiArray);
+
 /*
 
 Given an array [a1, a2, ..., aN, b1, b2, ..., bN, c1, c2, ..., cN] convert it to [a1, b1, c1, a2, b2, c2, ..., aN, bN, cN]
 
 */
-var givenArray = ['a1', 'a2', '...', 'aN', 'b1', 'b2', '...', 'bN', 'c1', 'c2', '...', 'cN'];
+var givenArray = ['a1', 'a2', 'aN', 'b1', 'b2', 'bN', 'c1', 'c2', 'cN'];
 
 
 function sortArray(array) {
     var As = [];
     var Bs = [];
     var Cs = [];
-    var dots = [];
     for(var i = 0; i < array.length; i++) {
         if(array[i].charAt(0) === 'a') {
             As.push(array[i]);
@@ -72,15 +71,9 @@ function sortArray(array) {
             Cs.push(array[i]);
         }
     }
-    for(var i = 0; i < array.length; i++) {
-        if(array[i].charAt(0) === '.') {
-           dots.push(array[i]);
-           dots.splice(1, 2);
-        }
-    }
     var order = [];
     for(i = 0; i < As.length; i++){
-        order.push(As[i], Bs[i], Cs[i], dots[i]);
+        order.push(As[i], Bs[i], Cs[i]);
     }
     return order;
 }
@@ -92,9 +85,22 @@ sortArray(givenArray);
 There is an array of non-negative integers. A second array is formed by shuffling the elements of the first array and deleting a random element. Given these two arrays, find which element is missing in the second array.
 
 */
+var array1 = [1, 2, 3, 4, 5, 6, 7, 8];
+var array2 = [7, 5, 2, 6, 1, 3, 4];
 
+function compareArrays(arrayone, arraytwo) {
+    var order = arraytwo.sort();
+    for(var i = 0; i < arrayone.length; i++) {
+        for(var j = 0; j < order.length; j++) {
+            if(arrayone[i] !== order[j]) {
+                var missingElement = arrayone[i];
+            }
+        }
+    }
+    console.log("the missing element in the second array is " + missingElement);
+}
 
-
+compareArrays(array1, array2);
 
 
 /*
@@ -108,8 +114,28 @@ longestWords("I gave a present to my parents") // ["present", "parents"]
 longestWords("Buffalo buffalo Buffalo buffalo buffalo buffalo Buffalo buffalo") // ["buffalo"] or ["Buffalo"]
 
 */
+var mySentence = "If I were the longest word in this sentence what word would I be?";
 
+function longestWord(str) {
+    var length = 0;
+    var word;
+    var splitWords = str.split(' ');
+    var lWArray = [];
+    for(var i = 0; i < splitWords.length; i++) {
+        if(splitWords[i].length >= length) {
+            length = splitWords[i].length;
+        }
+    }
+    for(var j = 0; j < splitWords.length; j++) {
+        if(splitWords[j].length === length) {
+            word = splitWords[j];
+            lWArray.push(word);
+        }
+    }
+    return lWArray;
+}
 
+longestWord(mySentence);
 /*
 
 If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
@@ -117,15 +143,38 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 Find the sum of all the multiples of 3 or 5 below 1000.
 
 */
+function multiples() {
+    var count = 0;
+    for(var i = 0; i <= 1000; i++)
+        if(i % 3 === 0 || i % 5 === 0) {
+            count += i;
+        }
+    return count;
+}
 
+multiples();
 
 /*
 
 Remove duplicate characters in a given string keeping only the first occurrences. For example, if the input is ‘tree traversal’ the output will be "tre avsl".
 
 */
+var duplicateCharacters = "I wonder what this sentence will look like once I remover all the duplicate characters";
 
 
+function noDuplicates(str) {
+    var separator = str.split('');
+    for(var i = separator.length; i > 0; i--) {
+        if(separator.indexOf(separator[i]) !== i) {
+            separator.splice(i, 1);
+            }
+        }
+    return separator.join('');
+} 
+
+
+
+noDuplicates(duplicateCharacters);
 
 /*
 Write a sum method which will work properly when invoked using either syntax below.
@@ -134,3 +183,16 @@ console.log(sum(2,3));   // Outputs 5
 console.log(sum(2)(3));  // Outputs 5
 
 */
+
+function sum(num1, num2) {
+    if(!isNaN(num1) && !isNaN(num2)){
+    return num1 + num2;
+    } else {
+        return function(num2) {
+            return num1 + num2;
+        };
+    }
+}
+
+console.log(sum(2,3));
+console.log(sum(2)(3));
